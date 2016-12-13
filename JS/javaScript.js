f618780707cbf41e8e87c1204d7d30d0890c3ec4
Maha Matin <matin643@gmail.com>
2016-12-12 22:10:27 -0800

    Define variables & loop through slides

---
+++ JS/javaScript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  //declare variable:
+  var totalWidth = 0;
+  var positions = new Array();
+
+  //make a loop through slides using each
+  $('#slides .slide').each(function(i){
+  
+
+  });
+
+});
